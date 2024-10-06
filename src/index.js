cd20ffa56b6ca3208a3230528b13b96071cfb64f
Suryakumar Selvakumar <ssuryakumar2077@gmail.com>
2024-10-05 21:13:48 -0600

    feat(main): Finish main section of the project
    Create the My Work section with the project cards with their own content
    and style everything appropriately

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-
 import "./style.css";
+import externalLinkIcon from "./icons/open-in-new.svg";
+import gitHubIcon from "./icons/github.svg";
+
+const projectDemoLinkImg = document.querySelectorAll("#demo");
+for (const item of projectDemoLinkImg) {
+  item.src = externalLinkIcon;
+}
+
+const projectGitHubImg = document.querySelectorAll("#github");
+for (const item of projectGitHubImg) {
+  item.src = gitHubIcon;
+}
